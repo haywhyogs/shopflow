fa155dce1795ac8ff089cef51fa6d9f888ec0454
Ayodeji Ogunsola <haywhyogs1@gmail.com>
2026-06-27 05:13:59 -0700

    fix: replace ssh deploy with az vm run-command via custom role

--- terraform/identity.tf
+++ terraform/identity.tf
@@ -43,3 +43,27 @@ resource "azurerm_role_assignment" "github_actions_acr_push" {
   role_definition_name = "AcrPush"
   principal_id         = data.azuread_service_principal.github_actions.object_id
 }
+
+resource "azurerm_role_definition" "vm_run_command" {
+  name        = "ShopFlow VM Run Command"
+  scope       = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  description = "Allows GitHub Actions to execute run-command on ShopFlow VM only"
+
+  permissions {
+    actions = [
+      "Microsoft.Compute/virtualMachines/runCommand/action",
+      "Microsoft.Compute/virtualMachines/read"
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  ]
+}
+
+resource "azurerm_role_assignment" "github_actions_run_command" {
+  scope              = azurerm_linux_virtual_machine.main.id
+  role_definition_id = azurerm_role_definition.vm_run_command.role_definition_resource_id
+  principal_id       = data.azuread_service_principal.github_actions.object_id
+}
